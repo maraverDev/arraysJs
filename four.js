d9544e558dcc9a96abfbecbc221c081db4594141
maraverDev <maraverdev@gmail.com>
2024-12-10 01:15:00 +0100

    Exercise four finished

---
+++ four.js
@@ -0,0 +1,24 @@
+const array = ["Gonzalo", "Pepe", "Pene", "Pito", "Cuadro"];
+let arrayHecho = array.filter((a) => a.length >= 5);
+console.log (arrayHecho);
+// [ 'Gonzalo', 'Cuadro' ]
+
+// Invierte las letras de cada palabra utilizando map y split/reverse/join.
+
+let invertido = array.map(palabra => palabra.split('').reverse('').join(''));
+console.log(invertido);
+// [ 'olaznoG', 'epeP', 'eneP', 'otiP', 'ordauC' ] 
+
+// Ordena las palabras por longitud usando una función personalizada en sort.
+
+function ordenarPalabras(array) {
+    array.sort((a,b) => b.length - a.length);
+    return array;
+}
+
+console.log(ordenarPalabras(array));
+// [ 'Gonzalo', 'Cuadro', 'Pepe', 'Pene', 'Pito' ]
+
+
+
+
